c74835948df3d8ec06707ccf2b8b1c2d724e430c
Francesco Barrale <francesco.barrale@mail.polimi.it>
2026-04-01 19:09:23 +0200

    PROVA BRANCH

--- main.cpp
+++ main.cpp
@@ -4,3 +4,11 @@ int main(){
 	std::cout << "CIAO FRANCESCO"<<std::endl;
 	return 0;
 }
+
+void stampa(){
+	std::cout<<"Sto stampando CIAO"<<std::endl;
+}
+
+void stampa2(){
+	std::cout<<"Sto stampando CIAO"<<std::endl;
+}
